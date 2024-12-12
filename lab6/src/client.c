0cfbb527ec98af7d26e85cb1c0a8cc5f021344fd
ZhukovMikhail32 <zhukovmik230604@gmail.com>
2024-12-12 22:54:37 +0000

    client.c

--- lab6/src/client.c
+++ lab6/src/client.c
@@ -1,10 +1,34 @@
-#include <pthread.h>
-#include "utils.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <errno.h>
+#include <getopt.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 struct Server {
   char ip[255];
   int port;
 };
+
+uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod) {
+  uint64_t result = 0;
+  a = a % mod;
+  while (b > 0) {
+    if (b % 2 == 1)
+      result = (result + a) % mod;
+    a = (a * 2) % mod;
+    b /= 2;
+  }
+
+  return result % mod;
+}
 
 struct ThreadArgs {
   struct Server server;
